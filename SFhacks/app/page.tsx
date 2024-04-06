e7e501e9a97df7de81031b600f8dc2293bfeeca6
EtienneDevictor <DevictorEtienne@gmail.com>
2024-04-05 22:04:51 -0700

    added basic page structure

--- SFhacks/app/page.tsx
+++ SFhacks/app/page.tsx
@@ -1,12 +1,20 @@
 import Image from "next/image";
-import clientPromise from "./data/mongodb";
+import Link from 'next/link';
+
 
 export default function Home() {
   return (
     // create a basic home page
-    <main>
-      <h1>Welcome to the home page</h1>
-      <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+    <main >
+      <div className="w-1/3 h-1 ">
+        <Link href="/NewStory"></Link>
+        <Link href="/ContinueStory"></Link>
+        <Link href="/Characters"></Link>
+      </div>
+      <div className="w-2/3 h-1 opacity-25">
+        <Image src="" alt="Picture of Book"></Image>
+        <h1> Your Story </h1>
+      </div>
     </main>
   );
 }
